09648b329f9ffaa0bb903917ad8bc824c7785ea1
oo1991 <87750941+oo1991@users.noreply.github.com>
2025-02-24 16:39:39 +0200

    Update get_fear.py

--- get_fear.py
+++ get_fear.py
@@ -170,7 +170,7 @@ def get_fear_and_greed_index_cryptorank():
     driver.implicitly_wait(10)
     
     try:
-        fng_element = driver.find_element(By.XPATH, "//p[@class='sc-56567222-0 sc-ee1f942b-1 fzulHc kwaTsJ']")
+        fng_element = driver.find_element(By.XPATH, "//p[@class='sc-dec2158d-0 sc-ee1f942b-1 gKONrN kwaTsJ']")
         fng_index = fng_element.text
         return fng_index
     except Exception as e:
